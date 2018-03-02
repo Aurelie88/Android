08d5e5118ebd840e323cb1f59812062925fcc5d6
Damien <damien.gaiga@gmail.com>
2018-03-02 19:27:50 +0100

    Version finale Correctif sur promotion
    A faire:
    Tests plus poussés (partie promotion)
    Ajout d'un écran pour visualiser les informations d'une promotion
    Gros nettoyage du code et optimisation (suppression redondance de code)
    Partie Clients et ventes si le temps...

--- app/src/main/java/com/iut/ecommerce/ecommerce/fragment/CategorieView.java
+++ app/src/main/java/com/iut/ecommerce/ecommerce/fragment/CategorieView.java
@@ -130,7 +130,7 @@ public class CategorieView extends Fragment implements ActiviteEnAttenteAvecResu
 
         } else if ("nok".equals("nok")) {
             Log.i("_S", "erreur surpression");
-            //Toast.makeText(getContext(), "La catégorie contient encore des articles", Toast.LENGTH_LONG).show();
+            Toast.makeText(getContext(), "La catégorie contient encore des articles", Toast.LENGTH_LONG).show();
 
         } else {
             Log.i("_S", "autre erreur");
